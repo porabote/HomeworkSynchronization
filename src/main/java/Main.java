ed6dc2bfc649dba2085862e7c760a6fa89fd8fd7
maksimov <dmaksimov@vtb.ru>
2023-05-16 19:51:52 +0300

    max value

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,26 @@ public class Main {
 
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
     private static int maxRoutesCount = 1000;
-    private static int countFinishedProcesses = 0;
+    private static int maxRepeatedValue = 0;
 
     public static void main(String[] args) throws InterruptedException {
+
+
+        Thread notificator = new Thread(() -> {
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+                    System.out.println("Current max value " + maxRepeatedValue + " - " + sizeToFreq.get(maxRepeatedValue));
+                }
+            }
+        });
+        notificator.start();
+
+
 
         Runnable action = () -> {
 
@@ -26,49 +43,46 @@ public class Main {
                     sizeToFreq.put(countRightRepeated, sizeToFreq.get(countRightRepeated) + 1);
                 }
 
-                countFinishedProcesses++;
-                System.out.println(countFinishedProcesses);
-                if (countFinishedProcesses == maxRoutesCount) {
-                    sizeToFreq.notify();
+                if (maxRepeatedValue == 0) {
+                    maxRepeatedValue = countRightRepeated;
+                } else if (sizeToFreq.get(countRightRepeated) > sizeToFreq.get(maxRepeatedValue)) {
+                    maxRepeatedValue = countRightRepeated;
                 }
+
+                sizeToFreq.notify();
+
+//                countFinishedProcesses++;
+//                System.out.println(countFinishedProcesses);
+//                if (countFinishedProcesses == maxRoutesCount) {
+//                    sizeToFreq.notify();
+//                }
             }
-
         };
-
 
         for (int i = 0; i < maxRoutesCount; i++) {
             Thread thread = new Thread(action);
             thread.start();
+            thread.join();
         }
 
+        notificator.interrupt();
+
+
 
         new Thread(() -> {
 
-            synchronized (sizeToFreq) {
-                try {
-                    sizeToFreq.wait();
-                } catch (InterruptedException e) {
-                    return;
-                }
-
-                System.out.println(sizeToFreq);
-            }
+            System.out.println(sizeToFreq);
+//            synchronized (sizeToFreq) {
+//                try {
+//                    sizeToFreq.wait();
+//                } catch (InterruptedException e) {
+//                    return;
+//                }
+//
+//                System.out.println(sizeToFreq);
+//            }
 
         }).start();
-
-        new Thread(() -> {
-            while (!Thread.interrupted()) {
-                synchronized (sizeToFreq) {
-                    try {
-                        sizeToFreq.wait();
-                        System.out.println();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-
-                }
-            }
-        });
 
     }
 
